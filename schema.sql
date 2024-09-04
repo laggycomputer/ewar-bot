04ab2a521fa5d7fa34fcf6d64af6aa0262d8af8f
Dante Dam <laggycomputer@yahoo.com>
2024-09-03 23:25:51 -0700

    continue developing model; rating is now bot-wide

--- schema.sql
+++ schema.sql
@@ -1,18 +1,16 @@
 CREATE TABLE IF NOT EXISTS players
 (
-    guild_id    BIGINT       NOT NULL,
     player_id   INT          NOT NULL,
     player_name VARCHAR(100) NOT NULL,
-    PRIMARY KEY (guild_id, player_id),
-    UNIQUE (guild_id, player_name)
+    PRIMARY KEY (player_id),
+    UNIQUE (player_name)
 );
 
 CREATE TABLE IF NOT EXISTS player_discord
 (
-    guild_id        BIGINT NOT NULL,
     player_id       INT    NOT NULL,
     discord_user_id BIGINT NOT NULL,
-    FOREIGN KEY (guild_id, player_id) REFERENCES players (guild_id, player_id)
+    FOREIGN KEY (player_id) REFERENCES players (player_id)
 );
 
-CREATE INDEX discord_user_id ON player_discord (discord_user_id, guild_id);
+CREATE INDEX discord_user_id ON player_discord (discord_user_id);
--- src/model.rs
+++ src/model.rs
@@ -1,8 +1,12 @@
 use mongodb::bson::serde_helpers::serialize_bson_datetime_as_rfc3339_string;
 use mongodb::bson::DateTime;
 use serde::{Deserialize, Serialize};
+use serenity::all::UserId;
+use skillratings::trueskill::TrueSkillRating;
+use std::collections::{HashMap, HashSet};
 
-type PlayerID = (i64, i32);
+type PlayerID = UserId;
+type GameID = u64;
 
 #[derive(Serialize, Deserialize)]
 struct Game {
@@ -13,14 +17,30 @@ struct Game {
     length: u32,
     // time submitted to system
     #[serde(serialize_with = "serialize_bson_datetime_as_rfc3339_string")]
-    time: DateTime,
+    when: DateTime,
 }
 
-enum StandingEvent {
+enum StandingEventType {
     // remove rating for foul play
-    Penalty { affected: PlayerID, amount: f64, reason: String },
+    Penalty { amount: f64, reason: String },
     // add deviation for inactivity
-    InactivityDecay { affected: PlayerID, amount: f64 },
+    InactivityDecay { amount: f64 },
     // regular game
-    GameEnd { game: u64 },
+    GameEnd { game: Game },
+}
+
+type EventNumber = u32;
+
+struct StandingEvent {
+    number: EventNumber,
+    affected: HashSet<PlayerID>,
+    event_type: StandingEventType,
+    when: DateTime,
+}
+
+// precompute rating at certain points in the timeline
+struct Checkpoint {
+    after: EventNumber,
+    // standings changed since last checkpoint
+    updates: HashMap<PlayerID, TrueSkillRating>,
 }
