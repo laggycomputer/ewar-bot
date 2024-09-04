0531144cc9a22493617cd4ec22006255aaebb21c
Dante Dam <laggycomputer@yahoo.com>
2024-09-03 23:26:30 -0700

    player_discord entries are unique

--- schema.sql
+++ schema.sql
@@ -10,7 +10,8 @@ CREATE TABLE IF NOT EXISTS player_discord
 (
     player_id       INT    NOT NULL,
     discord_user_id BIGINT NOT NULL,
-    FOREIGN KEY (player_id) REFERENCES players (player_id)
+    FOREIGN KEY (player_id) REFERENCES players (player_id),
+    UNIQUE (player_id, discord_user_id)
 );
 
 CREATE INDEX discord_user_id ON player_discord (discord_user_id);
