600ee826b560fdc87ea125056085a398b853de01
Dante Dam <laggycomputer@yahoo.com>
2024-09-03 20:05:32 -0700

    switch to yaml config

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,31 @@ mod util;
 mod commands;
 
 use crate::commands::meta::ping;
-use dotenv::dotenv;
+use clap::ValueHint;
 use poise::FrameworkOptions;
 use serenity::all::GatewayIntents;
 use serenity::all::GuildId;
 use serenity::Client;
-use std::env;
+use std::fs;
+use std::path::PathBuf;
+use yaml_rust2::YamlLoader;
 
 struct BotVars {}
 
 #[tokio::main]
 async fn main() {
-    dotenv().ok();
+    let cmd = clap::command!("ewar-bot")
+        .about("Discord bot for handling ranked Egyptian War backed by TrueSkill")
+        .arg(clap::arg!(<"config"> "config file path")
+            .value_parser(clap::value_parser!(PathBuf))
+            .value_hint(ValueHint::FilePath));
+
+    let args = cmd.get_matches();
+
+    let config_path = args.get_one::<PathBuf>("config").expect("config file is bad path?");
+    let config_doc = &YamlLoader::load_from_str(
+        &*fs::read_to_string(config_path).expect("can't open config file")
+    ).expect("can't parse config file")[0];
 
     let framework = poise::Framework::<BotVars, BotError>::builder()
         .options(FrameworkOptions {
@@ -26,12 +39,13 @@ async fn main() {
                 poise::builtins::register_globally(ctx, &framework.options().commands).await?;
                 println!("registered commands globally");
                 poise::builtins::register_in_guild(ctx, &framework.options().commands, GuildId::from(576979941304827954)).await?;
+                println!("registered commands locally");
                 Ok(BotVars {})
             })
         })
         .build();
 
-    let token = env::var("token").expect("discord client token missing");
+    let token = config_doc["token"].as_str().expect("discord client token missing");
     let mut client = Client::builder(&token, GatewayIntents::all())
         .event_handler(handler::EWarBotHandler)
         .framework(framework)
