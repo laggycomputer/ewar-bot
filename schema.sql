cd35ee0488c34109dae7fec4fafb2421a4cb7e88
Dante Dam <laggycomputer@yahoo.com>
2024-09-03 21:18:33 -0700

    add sql schema

---
+++ schema.sql
@@ -0,0 +1,31 @@
+CREATE TABLE IF NOT EXISTS blacklist
+(
+    user_id BIGINT UNIQUE PRIMARY KEY NOT NULL,
+    mod_id  BIGINT                    NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS guilds
+(
+    guild_id           BIGINT UNIQUE PRIMARY KEY NOT NULL,
+    prefix             VARCHAR(10),
+    custom_dollar_sign VARCHAR(30)
+);
+
+CREATE TABLE IF NOT EXISTS players
+(
+    guild_id    BIGINT       NOT NULL,
+    player_id   INT          NOT NULL,
+    player_name VARCHAR(100) NOT NULL,
+    PRIMARY KEY (guild_id, player_id),
+    UNIQUE (guild_id, player_name)
+);
+
+CREATE TABLE IF NOT EXISTS player_discord
+(
+    guild_id        BIGINT NOT NULL,
+    player_id       INT    NOT NULL,
+    discord_user_id BIGINT NOT NULL,
+    FOREIGN KEY (guild_id, player_id) REFERENCES players (guild_id, player_id)
+);
+
+CREATE INDEX discord_user_id ON player_discord (discord_user_id, guild_id);
