c0d5d550a5096f3ac9c1628ddd5b64f30b5f049d
Dante Dam <laggycomputer@yahoo.com>
2024-09-09 22:52:31 -0700

    send response when league moderator check fails

--- src/util/checks.rs
+++ src/util/checks.rs
@@ -1,5 +1,11 @@
 use crate::{BotError, Context};
 
 pub(crate) async fn is_league_moderator(ctx: Context<'_>) -> Result<bool, BotError> {
-    Ok(ctx.author().id.get() == 328678556899213322)
+    let cond = ctx.author().id.get() == 328678556899213322;
+
+    if !cond {
+        ctx.reply(":x: must be league moderator to do this").await?;
+    }
+
+    Ok(cond)
 }
