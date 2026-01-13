8a962b4f8c1713aab23898c73cbbfac3b6292a7b
Dante Dam <laggycomputer@yahoo.com>
2026-01-13 00:53:49 -0800

    missing semicolon

--- src/commands/ewar/user.rs
+++ src/commands/ewar/user.rs
@@ -79,7 +79,7 @@ async fn display_lookup_result(ctx: Context<'_>, looked_up: Player) -> Result<()
     let mut consec_decay = 0;
     for event in events {
         if let InactivityDecay { .. } = &event.inner {
-            consec_decay += 1
+            consec_decay += 1;
         } else {
             match consec_decay {
                 0 => {}
