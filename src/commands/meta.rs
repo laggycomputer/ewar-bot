777fc1bb05ebb09df46baed254d5fecab881fd7b
Dante Dam <laggycomputer@yahoo.com>
2026-01-13 00:42:15 -0800

    inline format!

--- src/commands/meta.rs
+++ src/commands/meta.rs
@@ -13,7 +13,7 @@ pub(crate) async fn ping(ctx: Context<'_>) -> Result<(), BotError> {
     let ping_num = ctx.ping().await.as_millis();
     ctx.say(match ping_num {
         0 => String::from("ok, waiting for more data to report ping"),
-        _ => format!("hi, heartbeat is pinging in {} ms", ping_num),
+        _ => format!("hi, heartbeat is pinging in {ping_num} ms"),
     })
     .await?;
     Ok(())
